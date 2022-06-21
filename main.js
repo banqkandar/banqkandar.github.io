e5541867f49ed6baf60ee79a2dcede8192a19885
Bangkandar <sharepelajaran@gmail.com>
2022-06-21 11:56:46 +0700

    ubah codingan theme dark

--- main.js
+++ main.js
@@ -21,6 +21,6 @@ if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
 
     if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
+        toggleSwitch.checked = false;
     }
 }
